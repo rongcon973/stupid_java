e86f695f3a39f5f337a90d603babc9e4b37a2d2a
51600100 <43369623+rongcon973@users.noreply.github.com>
2018-10-15 21:21:41 +0700

    Stupid.java
    New file

--- src/stupid/x.java
+++ src/stupid/x.java
@@ -3,22 +3,22 @@ import java.io.IOException;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-public class x {
+public class Stupid {
     public static void main(String[] arg)   {        
-        int CC=hohoho(arg);
-        System.exit(CC);
+        int result = hohoho(arg);
+        System.exit(result);
     }
-    public static int hohoho(String uuu[]) {
+    public static int hohoho(String arr[]) {
         try{
-            if(uuu.length != 2) 
+            if(arr.length != 2) 
                 return -1;
-            String bbb,ddd= new String(Files.readAllBytes(Paths.get(uuu[1]))); 
-            bbb= new String(Files.readAllBytes(Paths.get(uuu[0])));
-            int kkk = bbb.length();
-            if(kkk<4) 
+            String bbb,ddd= new String(Files.readAllBytes(Paths.get(arr[1]))); 
+            bbb= new String(Files.readAllBytes(Paths.get(arr[0])));
+            int size = bbb.length();
+            if(size<4) 
                 return -2;
             int RRR=0; float eee=0;
-            for(int aaa=0;aaa<kkk-3;aaa++)
+            for(int aaa=0;aaa<size-3;aaa++)
             {       
                 for(int qqq=0;qqq<ddd.length()-4;qqq+=1) {
                     int vOv = 0;
